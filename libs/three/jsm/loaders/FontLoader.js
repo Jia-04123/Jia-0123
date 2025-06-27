eb819de3edaae7a01b96310f6677d9c186d728e4
Jia-04123 <lcheajia04123@gmail.com>
2025-06-27 23:36:29 +0800

    Update FontLoader.js

--- libs/three/jsm/loaders/FontLoader.js
+++ libs/three/jsm/loaders/FontLoader.js
@@ -2,241 +2,146 @@ import {
 	FileLoader,
 	Loader,
 	ShapePath
-} from 'three';
+} from '../../../three/three.module.js';
 
-/**
- * A loader for loading fonts.
- *
- * You can convert fonts online using [facetype.js]{@link https://gero3.github.io/facetype.js/}.
- *
- * ```js
- * const loader = new FontLoader();
- * const font = await loader.loadAsync( 'fonts/helvetiker_regular.typeface.json' );
- * ```
- *
- * @augments Loader
- * @three_import import { FontLoader } from 'three/addons/loaders/FontLoader.js';
- */
 class FontLoader extends Loader {
 
-	/**
-	 * Constructs a new font loader.
-	 *
-	 * @param {LoadingManager} [manager] - The loading manager.
-	 */
-	constructor( manager ) {
-
-		super( manager );
-
+	constructor(manager) {
+		super(manager);
 	}
 
-	/**
-	 * Starts loading from the given URL and passes the loaded font
-	 * to the `onLoad()` callback.
-	 *
-	 * @param {string} url - The path/URL of the file to be loaded. This can also be a data URI.
-	 * @param {function(Font)} onLoad - Executed when the loading process has been finished.
-	 * @param {onProgressCallback} onProgress - Executed while the loading is in progress.
-	 * @param {onErrorCallback} onError - Executed when errors occur.
-	 */
-	load( url, onLoad, onProgress, onError ) {
-
+	load(url, onLoad, onProgress, onError) {
 		const scope = this;
 
-		const loader = new FileLoader( this.manager );
-		loader.setPath( this.path );
-		loader.setRequestHeader( this.requestHeader );
-		loader.setWithCredentials( this.withCredentials );
-		loader.load( url, function ( text ) {
+		const loader = new FileLoader(scope.manager);
+		loader.setPath(scope.path);
+		loader.setRequestHeader(scope.requestHeader);
+		loader.setWithCredentials(scope.withCredentials);
+		loader.load(url, function (text) {
+			try {
+				onLoad(scope.parse(JSON.parse(text)));
+			} catch (e) {
+				if (onError) {
+					onError(e);
+				} else {
+					console.error(e);
+				}
 
-			const font = scope.parse( JSON.parse( text ) );
-
-			if ( onLoad ) onLoad( font );
-
-		}, onProgress, onError );
-
+				scope.manager.itemError(url);
+			}
+		}, onProgress, onError);
 	}
 
-	/**
-	 * Parses the given font data and returns the resulting font.
-	 *
-	 * @param {Object} json - The raw font data as a JSON object.
-	 * @return {Font} The font.
-	 */
-	parse( json ) {
-
-		return new Font( json );
-
+	parse(json) {
+		const font = new Font(json);
+		return font;
 	}
-
 }
 
-/**
- * Class representing a font.
- */
 class Font {
 
-	/**
-	 * Constructs a new font.
-	 *
-	 * @param {Object} data - The font data as JSON.
-	 */
-	constructor( data ) {
-
-		/**
-		 * This flag can be used for type testing.
-		 *
-		 * @type {boolean}
-		 * @readonly
-		 * @default true
-		 */
-		this.isFont = true;
-
-		this.type = 'Font';
-
-		/**
-		 * The font data as JSON.
-		 *
-		 * @type {Object}
-		 */
+	constructor(data) {
 		this.data = data;
-
+		this.type = 'Font';
 	}
 
-	/**
-	 * Generates geometry shapes from the given text and size. The result of this method
-	 * should be used with {@link ShapeGeometry} to generate the actual geometry data.
-	 *
-	 * @param {string} text - The text.
-	 * @param {number} [size=100] - The text size.
-	 * @return {Array<Shape>} An array of shapes representing the text.
-	 */
-	generateShapes( text, size = 100 ) {
-
+	generateShapes(text, size = 100) {
 		const shapes = [];
-		const paths = createPaths( text, size, this.data );
 
-		for ( let p = 0, pl = paths.length; p < pl; p ++ ) {
+		const paths = createPaths(text, size, this.data);
 
-			shapes.push( ...paths[ p ].toShapes() );
-
+		for (let p = 0, pl = paths.length; p < pl; p++) {
+			shapes.push(...paths[p].toShapes(false));
 		}
 
 		return shapes;
-
 	}
-
 }
 
-function createPaths( text, size, data ) {
-
-	const chars = Array.from( text );
+// helper for Font
+function createPaths(text, size, data) {
 	const scale = size / data.resolution;
-	const line_height = ( data.boundingBox.yMax - data.boundingBox.yMin + data.underlineThickness ) * scale;
+	const lineHeight = (data.boundingBox.yMax - data.boundingBox.yMin + data.underlineThickness) * scale;
 
 	const paths = [];
 
-	let offsetX = 0, offsetY = 0;
+	const offsetX = 0;
+	let offsetY = 0;
 
-	for ( let i = 0; i < chars.length; i ++ ) {
+	for (let i = 0; i < text.length; i++) {
+		const char = text[i];
 
-		const char = chars[ i ];
-
-		if ( char === '\n' ) {
-
+		if (char === '\n') {
 			offsetX = 0;
-			offsetY -= line_height;
-
+			offsetY -= lineHeight;
 		} else {
+			const path = createPath(char, data, scale, offsetX, offsetY);
 
-			const ret = createPath( char, scale, offsetX, offsetY, data );
-			offsetX += ret.offsetX;
-			paths.push( ret.path );
+			paths.push(path);
 
+			offsetX += path.offset;
 		}
-
 	}
 
 	return paths;
-
 }
 
-function createPath( char, scale, offsetX, offsetY, data ) {
+// helper for Font
+function createPath(char, data, scale, offsetX, offsetY) {
+	const glyph = data.glyphs[char] || data.glyphs['?'];
 
-	const glyph = data.glyphs[ char ] || data.glyphs[ '?' ];
-
-	if ( ! glyph ) {
-
-		console.error( 'THREE.Font: character "' + char + '" does not exists in font family ' + data.familyName + '.' );
-
+	if (!glyph) {
+		console.error(`FontLoader: character "${char}" does not exist in font.`);
 		return;
-
 	}
 
 	const path = new ShapePath();
 
-	let x, y, cpx, cpy, cpx1, cpy1, cpx2, cpy2;
+	let x = 0;
+	let y = 0;
 
-	if ( glyph.o ) {
+	if (glyph.o) {
+		const outline = glyph._cachedOutline || (glyph._cachedOutline = glyph.o.split(' '));
 
-		const outline = glyph._cachedOutline || ( glyph._cachedOutline = glyph.o.split( ' ' ) );
+		for (let i = 0; i < outline.length;) {
+			const action = outline[i++];
 
-		for ( let i = 0, l = outline.length; i < l; ) {
-
-			const action = outline[ i ++ ];
-
-			switch ( action ) {
-
-				case 'm': // moveTo
-
-					x = outline[ i ++ ] * scale + offsetX;
-					y = outline[ i ++ ] * scale + offsetY;
-
-					path.moveTo( x, y );
-
+			switch (action) {
+				case 'm': {
+					x = parseFloat(outline[i++]) * scale + offsetX;
+					y = parseFloat(outline[i++]) * scale + offsetY;
+					path.moveTo(x, y);
 					break;
-
-				case 'l': // lineTo
-
-					x = outline[ i ++ ] * scale + offsetX;
-					y = outline[ i ++ ] * scale + offsetY;
-
-					path.lineTo( x, y );
-
+				}
+				case 'l': {
+					x = parseFloat(outline[i++]) * scale + offsetX;
+					y = parseFloat(outline[i++]) * scale + offsetY;
+					path.lineTo(x, y);
 					break;
-
-				case 'q': // quadraticCurveTo
-
-					cpx = outline[ i ++ ] * scale + offsetX;
-					cpy = outline[ i ++ ] * scale + offsetY;
-					cpx1 = outline[ i ++ ] * scale + offsetX;
-					cpy1 = outline[ i ++ ] * scale + offsetY;
-
-					path.quadraticCurveTo( cpx1, cpy1, cpx, cpy );
-
+				}
+				case 'q': {
+					const cpx = parseFloat(outline[i++]) * scale + offsetX;
+					const cpy = parseFloat(outline[i++]) * scale + offsetY;
+					x = parseFloat(outline[i++]) * scale + offsetX;
+					y = parseFloat(outline[i++]) * scale + offsetY;
+					path.quadraticCurveTo(cpx, cpy, x, y);
 					break;
-
-				case 'b': // bezierCurveTo
-
-					cpx = outline[ i ++ ] * scale + offsetX;
-					cpy = outline[ i ++ ] * scale + offsetY;
-					cpx1 = outline[ i ++ ] * scale + offsetX;
-					cpy1 = outline[ i ++ ] * scale + offsetY;
-					cpx2 = outline[ i ++ ] * scale + offsetX;
-					cpy2 = outline[ i ++ ] * scale + offsetY;
-
-					path.bezierCurveTo( cpx1, cpy1, cpx2, cpy2, cpx, cpy );
-
+				}
+				case 'b': {
+					const cp1x = parseFloat(outline[i++]) * scale + offsetX;
+					const cp1y = parseFloat(outline[i++]) * scale + offsetY;
+					const cp2x = parseFloat(outline[i++]) * scale + offsetX;
+					const cp2y = parseFloat(outline[i++]) * scale + offsetY;
+					x = parseFloat(outline[i++]) * scale + offsetX;
+					y = parseFloat(outline[i++]) * scale + offsetY;
+					path.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
 					break;
-
+				}
 			}
-
 		}
-
 	}
 
-	return { offsetX: glyph.ha * scale, path: path };
-
+	return { offset: glyph.ha * scale, path };
 }
 
-export { FontLoader, Font };
+export { FontLoader };
